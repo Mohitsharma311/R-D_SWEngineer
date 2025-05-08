1de94391ca39d8cd8070abecce5903c5e45214cf
Mohit Sharma <mnsharma311@gmail.com>
2025-05-08 16:46:49 +0530

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+void setup() {
+    std::cout << "Enter a string: ";
+}
+
+void loop() {
+    std::string input;
+    std::getline(std::cin, input); 
+    std::reverse(input.begin(), input.end());
+    for (char &ch : input) {
+        ch = std::toupper(ch);
+    }
+    std::cout << "Reversed and Uppercased String: " << input << std::endl;
+}
+
+int main() {
+    setup();
+    while (true) {
+        loop();
+    }
+
+    return 0;
+}
