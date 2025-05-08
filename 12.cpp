53fb743c7436c207950839dab7ac5c412a1c8e88
Mohit Sharma <mnsharma311@gmail.com>
2025-05-08 16:49:35 +0530

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -1,1 +1,19 @@
+ #include <iostream>
+
+void checkEvenOdd(int num) {
+    if (num & 1) {
+        std::cout << num << " is Odd" << std::endl;
+    } else {
+        std::cout << num << " is Even" << std::endl;
+    }
+}
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    checkEvenOdd(number);
+
+    return 0;
+}
 
